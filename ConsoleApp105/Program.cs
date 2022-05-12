91bd47b94bf1115a806fba0a50f841503a633b4e
Olajumoke Sekoni <sekoniolajumoke@gmail.com>
2022-05-12 10:51:32 +0100

    Add project files.

---
+++ ConsoleApp105/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp105
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            //string a = @"C:\Users\MY PC\Desktop\jum";
+            //if(Directory.Exists(a))
+            //{
+            //    Directory.Delete(a);
+            //    Console.WriteLine("file deleted");
+
+            //}
+            //else
+            
+            //    Console.WriteLine("folder not found");
+
+            Console.WriteLine("enter a world: ");
+            string v = Console.ReadLine();
+            if (v==null)
+            {
+                Console.WriteLine("invalid");
+            }
+            else
+            {
+                if (Directory.Exists(v))
+                {
+                    Directory.Delete(v);
+                    Console.WriteLine("file deleted");
+
+                }
+                else
+                    Console.WriteLine("not found");
+            }
+        }
+
+        
+    }
+}
